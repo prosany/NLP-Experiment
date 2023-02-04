beb41607aabe52691da86ed703fd99d82c48d1b7
sany <mahabubsany2@gmail.com>
2023-02-05 03:37:00 +0600

    add

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-// const nlp = require("compromise");
+const nlp = require("compromise");
 
-// const commands = [
-//   {
-//     name: "greeting",
-//     phrases: ["hi", "hello", "hey"],
-//   },
-//   {
-//     name: "farewell",
-//     phrases: ["bye", "goodbye", "see you later"],
-//   },
-//   {
-//     name: "question",
-//     phrases: ["how are you", "what's up", "how do you do"],
-//   },
-// ];
+const commands = [
+  {
+    name: "greeting",
+    phrases: ["hi", "hello", "hey"],
+  },
+  {
+    name: "farewell",
+    phrases: ["bye", "goodbye", "see you later"],
+  },
+  {
+    name: "question",
+    phrases: ["how are you", "what's up", "how do you do"],
+  },
+  {
+    name: ["I'm fine, thanks for asking!", "I'm fine, whats up?"],
+    phrases: ["how are you", "what's up", "how do you do"],
+  },
+];
 
-// const processMessage = (message) => {
-//   let response = "";
-//   for (const command of commands) {
-//     for (const phrase of command.phrases) {
-//       if (nlp(message).has(phrase)) {
-//         response = `I recognize the "${command.name}" command.`;
-//         break;
-//       } else {
-//         response = `Sorry, I don't understand!`;
-//       }
-//     }
-//   }
-//   return response;
-// };
+const processMessage = (message) => {
+  let response = "";
+  for (const command of commands) {
+    for (const phrase of command.phrases) {
+      if (nlp(message).has(phrase)) {
+        if (Array.isArray(command.name)) {
+          const random = Math.floor(Math.random() * command.name.length);
+          const reply = command.name[random];
+          response = reply;
+          break;
+        }
+        response = `I recognize the "${command.name}" command.`;
+        break;
+      } else {
+        response = `Sorry, I don't understand!`;
+      }
+    }
+  }
+  return response;
+};
 
-// const message = "Hellos";
-// const response = processMessage(message);
-// console.log(response); // "I recognize the "greeting" command."
+const message = "how are you";
+const response = processMessage(message);
+console.log(response); // "I recognize the "greeting" command."
